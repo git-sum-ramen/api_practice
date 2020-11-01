e018409aa2e668897e03f7a292f59a361c410aa6
brian <riceb53@gmail.com>
2020-11-01 10:33:22 -0500

    add api key as environment variable

--- weather.rb
+++ weather.rb
@@ -3,10 +3,11 @@ require "http"
 p "What city would you like to know the weather of?"
 user_input = gets.chomp
 
-weather = HTTP.get("http://api.openweathermap.org/data/2.5/weather?q=#{user_input}&appid=__&units=imperial").parse
+weather = HTTP.get("http://api.openweathermap.org/data/2.5/weather?q=#{user_input}&appid=#{ENV['OPEN_WEATHER_API_KEY']}&units=imperial").parse
+
+# p weather
 
 temperature = weather['main']['temp']
-
 description = weather['weather'][0]['description']
 
 city = weather['name']
